a3bee6cef78393d2a082210fefe468c9370ce885
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-08-20 18:34:41 +0530

    added scroll to top

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,16 @@ import Navbar from '../components/Navbar';
 import { ThemeProvider } from 'next-themes';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from '../components/Footer';
+import GoTop from '../components/GoTop';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider attribute='class'>
+        <GoTop />
         <Navbar />
         <Component {...pageProps} />
-        <Footer/>
+        <Footer />
       </ThemeProvider>
     </>
   );
