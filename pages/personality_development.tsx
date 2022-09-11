4b8820d1ca0046691c200aeb47ff1a343033052b
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-09-11 13:35:09 +0530

    added image

--- pages/personality_development.tsx
+++ pages/personality_development.tsx
@@ -6,98 +6,27 @@ const personality_development = () => {
       <section className='text-gray-600 body-font'>
         <div className='container px-5 py-24 mx-auto'>
           <div className='flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4'>
-            <div className='p-4 md:w-1/3 sm:mb-0 mb-6'>
-              <div className='rounded-lg h-64 overflow-hidden'>
-                <img
-                  alt='content'
-                  className='hover:scale-110 transition-all duration-500 object-cover object-center h-full w-full'
-                  src='https://dummyimage.com/1203x503'
-                />
-              </div>
-              <h2 className='text-xl font-medium title-font text-gray-900 mt-5'>
-                Shooting Stars
-              </h2>
-              <p className='text-base leading-relaxed mt-2'>
-                Swag shoivdigoitch literally meditation subway tile tumblr
-                cold-pressed. Gastropub street art beard dreamcatcher neutra,
-                ethical XOXO lumbersexual.
-              </p>
-              <a className='text-red-500 inline-flex items-center mt-3'>
-                Learn More
-                <svg
-                  fill='none'
-                  stroke='currentColor'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  className='w-4 h-4 ml-2'
-                  viewBox='0 0 24 24'
-                >
-                  <path d='M5 12h14M12 5l7 7-7 7'></path>
-                </svg>
-              </a>
+            <div className='p-4 md:w-1/2 sm:mb-0 mb-6'>
+              
+                <div className='rounded-lg h-auto overflow-hidden'>
+                  <img
+                    alt='content'
+                    className='hover:scale-110 transition-all duration-500 object-cover object-center h-full w-full'
+                    src='/p_2.jpeg'
+                  />
+                </div>
+     
             </div>
-            <div className='p-4 md:w-1/3 sm:mb-0 mb-6'>
-              <div className='rounded-lg h-64 overflow-hidden'>
+         
+            <div className='p-4 md:w-1/2 sm:mb-0 mb-6'>
+              <div className='rounded-lg h-auto overflow-hidden'>
                 <img
                   alt='content'
                   className='hover:scale-110 transition-all duration-500 object-cover object-center h-full w-full'
-                  src='https://dummyimage.com/1204x504'
+                  src='/p_1.jpeg'
                 />
               </div>
-              <h2 className='text-xl font-medium title-font text-gray-900 mt-5'>
-                The Catalyzer
-              </h2>
-              <p className='text-base leading-relaxed mt-2'>
-                Swag shoivdigoitch literally meditation subway tile tumblr
-                cold-pressed. Gastropub street art beard dreamcatcher neutra,
-                ethical XOXO lumbersexual.
-              </p>
-              <a className='text-red-500 inline-flex items-center mt-3'>
-                Learn More
-                <svg
-                  fill='none'
-                  stroke='currentColor'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  className='w-4 h-4 ml-2'
-                  viewBox='0 0 24 24'
-                >
-                  <path d='M5 12h14M12 5l7 7-7 7'></path>
-                </svg>
-              </a>
-            </div>
-            <div className='p-4 md:w-1/3 sm:mb-0 mb-6'>
-              <div className='rounded-lg h-64 overflow-hidden'>
-                <img
-                  alt='content'
-                  className='hover:scale-110 transition-all duration-500 object-cover object-center h-full w-full'
-                  src='https://dummyimage.com/1205x505'
-                />
-              </div>
-              <h2 className='text-xl font-medium title-font text-gray-900 mt-5'>
-                The 400 Blows
-              </h2>
-              <p className='text-base leading-relaxed mt-2'>
-                Swag shoivdigoitch literally meditation subway tile tumblr
-                cold-pressed. Gastropub street art beard dreamcatcher neutra,
-                ethical XOXO lumbersexual.
-              </p>
-              <a className='text-red-500 inline-flex items-center mt-3'>
-                Learn More
-                <svg
-                  fill='none'
-                  stroke='currentColor'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  className='w-4 h-4 ml-2'
-                  viewBox='0 0 24 24'
-                >
-                  <path d='M5 12h14M12 5l7 7-7 7'></path>
-                </svg>
-              </a>
+              
             </div>
           </div>
         </div>
