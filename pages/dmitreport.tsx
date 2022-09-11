d13a1ad3f6dcf6a0ed56145a572403463c1652da
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-09-11 14:02:20 +0530

    added dmint main report

--- pages/dmitreport.tsx
+++ pages/dmitreport.tsx
@@ -3,6 +3,13 @@ import React from 'react';
 const Dmitreport = () => {
   return (
     <div>
+      <div className='rounded-lg overflow-hidden'>
+        <img
+          alt='content'
+          className='object-cover object-center h-full w-full'
+          src='/dmit_main.jpeg'
+        />
+      </div>
       <section className='text-gray-600 body-font'>
         <div className='container px-5 py-24 mx-auto'>
           <div className='flex flex-wrap -mx-4 mb-10 text-center'>
@@ -23,7 +30,6 @@ const Dmitreport = () => {
                   src='/d3.jpg'
                 />
               </div>
-
             </div>
           </div>
           <div className='flex flex-wrap -mx-4 -mb-10 text-center'>
@@ -35,7 +41,6 @@ const Dmitreport = () => {
                   src='/d8.jpg'
                 />
               </div>
-
             </div>
             <div className='sm:w-1/2 mb-10 px-4'>
               <div className='rounded-lg overflow-hidden'>
